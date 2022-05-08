4621d512ba34025f4b936c9031039b2d47c768d3
skyhightech55 <Mr.skys0711@softbank.ne.jp>
2022-05-08 09:19:44 +0900

    計画投稿・バリデーション及びエラーメッセージ表示の実装

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -3,4 +3,8 @@ class Plan < ApplicationRecord
   belongs_to :purpose
   belongs_to :who
   belongs_to :spot
+
+  validates :name, presence: true
+
+  validates :purpose_id, :who_id, :spot_id, numericality: { other_than: 1, message: "can't be blank" } 
 end
